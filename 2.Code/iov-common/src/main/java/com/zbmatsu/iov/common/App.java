9f483df6f7ae22b388e076138542152e8f64bbb5
janforp <janforp@163.com>
2016-06-19 14:34:59 +0800

    hehe1

--- 2.Code/iov-common/src/main/java/com/zbmatsu/iov/common/App.java
+++ 2.Code/iov-common/src/main/java/com/zbmatsu/iov/common/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hehe");
+        System.out.println("hehe1");
     }
     
     public static String getStr(){
